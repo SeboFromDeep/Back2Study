b0488a6bd300a25164bd7d86beeeab17953c29f9
Morwar22 <78794067+Morwar22@users.noreply.github.com>
2022-03-02 13:18:42 +0100

    Update config.js

--- Codigo/prueba/js/config.js
+++ Codigo/prueba/js/config.js
@@ -4,10 +4,10 @@
 
 module.exports ={
    databaseConfig:  {
-        host:"localhost",
-        user:"root",
-        password: "",
+        host:"back2study-database.cp7iwtc61ifu.eu-west-3.rds.amazonaws.com",
+        user:"admin",
+        password:"Back2Studyadmin",
         database:"back2study"
     } ,
-    puerto: 3000
+    puerto: 3306
 };
