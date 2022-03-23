21e16b6251ed9ffefcb2df79ff0bde47daa6763d
Morwar22 <alejmo12@ucm.es>
2022-03-23 13:46:58 +0100

    Esqueleto del ControllerCrearTarea

--- Codigo/controller/taskController.js
+++ Codigo/controller/taskController.js
@@ -46,9 +46,118 @@ class controllerT{
         }
     }
 
-    
+    createTareas(request, response)
+    {
+          
+          const errors = validationResult(request);
+          if (errors.isEmpty()) {
+              console.log("SIN ERRORES");
+            
+           
+            if(request.tipo=='programada')
+            {
 
-    
+                
+                let tareaProgramada = {
+                    nombre: request.body.nombre,
+                    prioridad: request.body.prioridad,
+                    categoria: request.body.categoria,
+                    usuario: request.session.currentId,
+                    fechaFin: request.body.fechaFin,
+                    fechaIni: request.body.fechaIni,
+
+                    //estos son los atributos de programada
+                    horasDia: request.body.horasDia,
+                    horasSemana: request.body.horasSemana
+                };
+                //ejecutamos la función registro de el DAO, y después se ejecuta cb_insert
+                //Aqui consultaríamos antes el primer hueco dispoible...
+
+                tareas.registroProgramada(cb_insert, tareaProgramada)
+              
+            }
+
+            else{
+
+              
+                let tareaManual = {
+                    nombre: request.body.nombre,
+                    prioridad: request.body.prioridad,
+                    categoria: request.body.categoria,
+                    usuario: request.session.currentId,
+                    fechaFin: request.body.fechaFin,
+                    fechaIni: request.body.fechaIni,
+                    horaIni: request.body.horaIni,
+                    horaFin: request.body.horaFin,
+                    recurrente: request.body.recurrente,
+                    diasRecurrentes: request.body.diasRecurrentes,
+
+                    
+
+                };
+
+               
+                tareas.consultarTarea(cb_consultarTarea, tareaManual);
+
+                function cb_consultarTarea(err, num_tareas){
+                    if(err){
+                        
+                        response.status(500);
+                       
+                    }
+                    else{
+
+                        if(num_tareas==0){
+                            
+                            tareas.registroManual(cb_insert, tareaManual)
+                        }
+                        else{
+                            //Lanzar error
+                        }
+                            
+                    }
+                }
+              
+            }
+              
+            function cb_insert(err, completed){
+                  if (err) {
+                      //console.log(err.message);
+                      response.status(500);
+                      let msg= "Error al añadir tarea";
+                      response.render("Añadir tarea", {   title: "¡Tarea erronea!",
+                                                      errores: errors.mapped(), 
+                                                      msgRegistro: msg});
+                  } 
+
+                  //cambios a partir de aqui para tareas.
+                  else {
+                      if(completed){
+                          console.log("Registro exitoso.")
+                          response.render("login", {  title: "Registro completado", 
+                                                  msgRegistro: "Registro completado " + usuario.nombre + ". Ya puedes loguearte.", 
+                                                  tipoAlert: "alert-success"});
+                      }
+                      else{
+                          let msg= "El usuario o correo ya existen.";
+                          console.log(msg);
+                          response.render("signup", {   title: "¡Registro erroneo Usu!",
+                                                          errores: errors.mapped(), 
+                                                          msgRegistro: msg});
+                      }
+                      
+                  }
+              }
+              
+          } 
+          else {
+              console.log("ERRORES!");
+              response.render("signup", {
+                  title: "¡Hay Errores!", 
+                  errores: errors.mapped(), 
+                  msgRegistro: false});
+          }
+      }
 }
 
 module.exports = controllerT;
