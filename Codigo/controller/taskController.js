e07bc4670dde1745f02f7c9fe69a5aba1f7a92e9
Gabriel Gomez <47831287+GabrielGzS@users.noreply.github.com>
2022-03-24 15:39:32 +0100

    Setup de negocio

--- Codigo/controller/taskController.js
+++ Codigo/controller/taskController.js
@@ -1,32 +1,28 @@
 "use strict";
 
-
-const config =require("../js/config");
+const config = require("../js/config");
 const mysql = require("mysql");
 
 const pool = mysql.createPool(config.databaseConfig);
-const DaoTask = require('../js/taskDAO');
-const tareas = new DaoTask(pool);
+const taskDao = require('../js/taskDAO');
+const daoTareas = new taskDao(pool);
 
 const moment = require('moment');
 const fecha = moment();
+
 //Validar
 const { check, validationResult } = require("express-validator");
 
-class controllerT{
+class controllerTareas {
 
-    getTareas(request, response){
+    listarTareas(request, response) {
 
-        tareas.listaTareas(cb_listaTareas, request.session.id_);
-        
-        function cb_listaTareas(err, tareas){
-            if(err){
-                
+        function listarTareasCallback(err, tareas) {
+            if (err) {
                 response.status(500);
-               
             }
-            else{
-                if(tareas){
+            else {
+                if (tareas) {
                     
                     // response.render("preguntas", {
                     //     consult: "Todas las Preguntas",
@@ -34,7 +30,7 @@ class controllerT{
                     //     imageUser: request.session.mailID, 
                     //     preguntas: preguntas
                     // });
-                    response.render("principal",{
+                    response.render("principal", {
                         title: "Inicio de sesión realizado con éxito", 
                         nameUser: request.session.userName, 
                         mailUser: request.session.mail,
@@ -44,11 +40,39 @@ class controllerT{
                 }
             }
         }
+
+        daoTareas.listaTareas(listarTareasCallback, request.session.id_);
     }
 
-    
+    añadirTareaManual(request, response) {
 
-    
+        function añadirTareaManualCallback(err, tarea) {
+            if (err) {
+                response.status(500);
+            }
+            else {
+                if (tarea) {
+                    console.log("Tarea Manual: ", tarea);
+                    
+                    request.session.id_ = tarea.id;
+                    request.session.mail = tarea.email;
+                    request.session.userName = tarea.username;
+
+                    response.locals.id_ = tarea.id_;
+                    response.locals.tarea = tarea;
+
+                    response.render("añadirTareas", {
+                        title: "Tarea Manual añadida con exito!", 
+                        nameUser: request.session.userName, 
+                        mailUser: request.session.mail,
+                        tarea: tarea
+                    })
+                }
+            }
+        }
+
+        daoTareas.añadirTareas(añadirTareaManualCallback, request.session.id_);
+    }
 }
 
-module.exports = controllerT;
+module.exports = controllerTareas;
