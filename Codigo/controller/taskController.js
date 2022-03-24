1b901404848f6de36e8ab9a035cb969e6684b350
Gabriel Gomez <47831287+GabrielGzS@users.noreply.github.com>
2022-03-24 21:27:53 +0100

    Controller merged

--- Codigo/controller/taskController.js
+++ Codigo/controller/taskController.js
@@ -14,7 +14,6 @@ const { createResponseLocals, createObjectFromRequest } = require("./controllerU
 
 //Validar
 const { check, validationResult } = require("express-validator");
-
 class controllerTareas {
 
     listarTareas(request, response) {
@@ -46,120 +45,6 @@ class controllerTareas {
         daoTareas.listaTareas(listarTareasCallback, request.session.id_);
     }
 
-  /*
-    createTareas(request, response)
-    {
-          
-          const errors = validationResult(request);
-          if (errors.isEmpty()) {
-              console.log("SIN ERRORES");
-            
-           
-            if(request.body.tipo=='programada')
-            {
-
-                
-                let tareaProgramada = {
-                    nombre: request.body.nombre,
-                    prioridad: request.body.prioridad,
-                    categoria: request.body.categoria,
-                    usuario: request.session.currentId,
-                    fechaFin: request.body.fechaFin,
-                    fechaIni: request.body.fechaIni,
-
-                    //estos son los atributos de programada
-                    horasDia: request.body.horasDia,
-                    horasSemana: request.body.horasSemana
-                };
-                //ejecutamos la función registro de el DAO, y después se ejecuta cb_insert
-                //Aqui consultaríamos antes el primer hueco dispoible...
-
-                tareas.registroProgramada(cb_insert, tareaProgramada)
-              
-            }
-
-            else{
-
-              
-                let tareaManual = {
-                    nombre: request.body.nombre,
-                    prioridad: request.body.prioridad,
-                    categoria: request.body.categoria,
-                    usuario: request.session.currentId,
-                    fechaFin: request.body.fechaFin,
-                    fechaIni: request.body.fechaIni,
-                    horaIni: request.body.horaIni,
-                    horaFin: request.body.horaFin,
-                    recurrente: request.body.recurrente,
-                    diasRecurrentes: request.body.diasRecurrentes,
-
-                    
-
-                };
-
-               
-                tareas.consultarTarea(cb_consultarTarea, tareaManual);
-
-                function cb_consultarTarea(err, num_tareas){
-                    if(err){
-                        
-                        response.status(500);
-                       
-                    }
-                    else{
-
-                        if(num_tareas==0){
-                            
-                            tareas.registroManual(cb_insert, tareaManual)
-                        }
-                        else{
-                            //Lanzar error
-                        }
-                            
-                    }
-                }
-              
-            }
-              
-            function cb_insert(err, completed){
-                  if (err) {
-                      //console.log(err.message);
-                      response.status(500);
-                      let msg= "Error al añadir tarea";
-                      response.render("Añadir tarea", {   title: "¡Tarea erronea!",
-                                                      errores: errors.mapped(), 
-                                                      msgRegistro: msg});
-                  } 
-
-                  //cambios a partir de aqui para tareas.
-                  else {
-                      if(completed){
-                          console.log("Registro exitoso.")
-                          response.render("login", {  title: "Registro completado", 
-                                                  msgRegistro: "Registro completado " + usuario.nombre + ". Ya puedes loguearte.", 
-                                                  tipoAlert: "alert-success"});
-                      }
-                      else{
-                          let msg= "El usuario o correo ya existen.";
-                          console.log(msg);
-                          response.render("signup", {   title: "¡Registro erroneo Usu!",
-                                                          errores: errors.mapped(), 
-                                                          msgRegistro: msg});
-                      }
-                      
-                  }
-              }
-              
-          } 
-          else {
-              console.log("ERRORES!");
-              response.render("signup", {
-                  title: "¡Hay Errores!", 
-                  errores: errors.mapped(), 
-                  msgRegistro: false});
-          }
-      }
-     */
     añadirTareaManual(request, response) {
         console.log("Añadiendo la tarea manual " + request.body.nombre + " a la BBDD");
 
@@ -171,7 +56,7 @@ class controllerTareas {
                 if (result) {
                     response.render("listar_tareas", createResponseLocals(true, "Exito: tarea manual añadida"));
                 } else {
-                    response.render("add_tarea_programada", createResponseLocals(false, "Error: tarea manual es null"));
+                    response.render("add_tarea_manual", createResponseLocals(false, "Error: tarea manual es null"));
                 }
             }
         }   
@@ -197,55 +82,129 @@ class controllerTareas {
         // comprobar que no haya tareas en la franja proporcionada
         // el dao automaticamente llamara a la funcion del DAO de añadir tareas si todo va bien
         daoTareas.consultarTareasEnFranjaHoraria(franjaHorariaCallback, request.body.fechaIni, request.body.fechaFin);
-  
-      addTareaProgramada(request, response){
-            console.log("Añadiendo la tarea " + request.body.nombre +  " a la BBDD");
-            let tareaProgramada = {
-                nombre: request.body.nombre,
-                prioridad: request.body.prioridad,
-                categoria: request.body.categoria.toUpperCase(),
-                usuario: request.session.currentId,
-                fechaFin: request.body.fechaFin,
-                fechaIni: request.body.fechaIni,
+    }
+      
+    addTareaProgramada(request, response){
+        console.log("Añadiendo la tarea " + request.body.nombre +  " a la BBDD");
+        
+        // inicializamos el objeto de tarea
+        request.body.category = request.body.categoria.toUpperCase()
+        request.body.tipo = request.body.tipo.toUpperCase()
+        let tareaProgramada = createObjectFromRequest(request);
 
-                //estos son los atributos de programada
-                horas: request.body.horas,
-                tipo: request.body.tipo.toUpperCase() // diaria o semanal 
-            };
+        // aquí planearíamos la tarea llamando al algoritmo de ordenación
 
-            // Aquí planearíamos la tarea llamando al algoritmo de ordenación
-
-            // Añadimos la tarea a la BBDD
-            tareas.añadirTareaProgramada(tareaProgramada, cb_addTareaProgramada);
-
-            function cb_addTareaProgramada(errors, result){
-                if (errors){
-                    //render y mssg pueden cambiar de nombre 
-                    response.render("add_tarea_programada", {
-                        title: "Error",
-                        mssg: "Error en la creación de la tarea",
-                        tipoAlert: "alert-danger" 
-                    });
-
+        function añadirTareaProgramadaCallback(errors, result){
+            if (errors){
+                //render y mssg pueden cambiar de nombre 
+                response.render("add_tarea_programada", createResponseLocals(false, "Error en la creación de la tarea"));
+            }
+            else {
+                if (result) {
+                    response.render("listar_tareas", createResponseLocals(true, "Tarea Programada añadida"));
                 }
-                else{
-                    if (completed){
-                        response.render("listar_tareas", {
-                            title: "Exito",
-                            mssg: "Tarea Programada añadida",
-                            tipoAlert: "alert-success" 
-                        });
-                    }
-                    else{
-                        response.render("add_tarea_programada", {
-                            title: "Error",
-                            mssg: "Error en la creación de la tarea",
-                            tipoAlert: "alert-danger" 
-                        });
-                    }
+                else { 
+                    response.render("add_tarea_programada", createResponseLocals(false, "Error en la creación de la tarea"));
                 }
             }
         }
+
+        // añadimos la tarea a la BBDD
+        tareas.añadirTareaProgramada(tareaProgramada, añadirTareaProgramadaCallback);
+    }
 }
 
 module.exports = controllerTareas;
+
+ /*
+    @Alex
+    añadirTareas(request, response) {
+        const errors = validationResult(request);
+        if (errors.isEmpty()) {
+            console.log("SIN ERRORES");          
+            if (request.body.tipo=='programada') {
+                let tareaProgramada = {
+                    nombre: request.body.nombre,
+                    prioridad: request.body.prioridad,
+                    categoria: request.body.categoria,
+                    usuario: request.session.currentId,
+                    fechaFin: request.body.fechaFin,
+                    fechaIni: request.body.fechaIni,
+
+                    //estos son los atributos de programada
+                    horasDia: request.body.horasDia,
+                    horasSemana: request.body.horasSemana
+                };
+                //ejecutamos la función registro de el DAO, y después se ejecuta cb_insert
+                //Aqui consultaríamos antes el primer hueco dispoible...
+
+                tareas.registroProgramada(cb_insert, tareaProgramada)
+            } else {
+                let tareaManual = {
+                    nombre: request.body.nombre,
+                    prioridad: request.body.prioridad,
+                    categoria: request.body.categoria,
+                    usuario: request.session.currentId,
+                    fechaFin: request.body.fechaFin,
+                    fechaIni: request.body.fechaIni,
+                    horaIni: request.body.horaIni,
+                    horaFin: request.body.horaFin,
+                    recurrente: request.body.recurrente,
+                    diasRecurrentes: request.body.diasRecurrentes,
+                };
+
+                tareas.consultarTarea(cb_consultarTarea, tareaManual);
+
+                function cb_consultarTarea(err, num_tareas){
+                    if (err) {
+                        response.status(500);
+                    } else {
+                        if (num_tareas == 0) {
+                            tareas.registroManual(cb_insert, tareaManual)
+                        }
+                        else {
+                            //Lanzar error
+                        }
+                    }
+                }
+            }
+              
+            function cb_insert(err, completed) {
+                if (err) {
+                    //console.log(err.message);
+                    response.status(500);
+                    response.render("Añadir tarea", {   
+                        title: "¡Tarea erronea!",
+                        errores: errors.mapped(), 
+                        msgRegistro: "Error al añadir tarea"});
+                    } 
+
+                 //cambios a partir de aqui para tareas.
+                else {
+                    if (completed) {
+                        console.log("Registro exitoso.")
+                        response.render("login", {  
+                            title: "Registro completado", 
+                            msgRegistro: "Registro completado " + usuario.nombre + ". Ya puedes loguearte.", 
+                            tipoAlert: "alert-success"});
+                    } else {
+                        let msg = "El usuario o correo ya existen.";
+                        console.log(msg);
+                        response.render("signup", {   
+                            title: "¡Registro erroneo Usu!",
+                            errores: errors.mapped(), 
+                                                          msgRegistro: msg});
+                      }
+                      
+                }
+            }
+              
+        } 
+    else {
+        console.log("ERRORES!");
+        response.render("signup", {
+            title: "¡Hay Errores!", 
+            errores: errors.mapped(), 
+            msgRegistro: false});
+        }
+    }*/
