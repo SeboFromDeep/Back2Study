9de777cc0f0a8775d176178941b9715e4313efa1
bea-espinar15 <beaesp01@ucm.es>
2022-04-07 20:55:18 +0200

    Tests

--- Codigo/TestsPiloto/Integracion/TIuserDAORegistro.js
+++ Codigo/TestsPiloto/Integracion/TIuserDAORegistro.js
@@ -1,39 +1,33 @@
 const assert = require("chai").assert;
 const chai = require("chai");
-const controller = require("../../controller/userController");
 const dao = require("../../js/userDAO");
 const mysql = require('mysql');
 const config = require('../../js/config');
-const task = new controller();
+const { NULL } = require("mysql/lib/protocol/constants/types");
 const pool = mysql.createPool(config.databaseConfig);
-const dao_aux = new dao(pool);
+const task = new dao(pool);
 
 describe('hooks', function () {
 
     after(function () {
-        // runs after each test in this block
-        dao_aux.delete_user("paco@gmail.com", function (error, result) { });
-        
+        // después de cada test borramos al que se ha insertado para poder ejecutarlos siempre
+        task.delete_user("paco@gmail.com", function (error, result) { });
     });
 
-    // test cases
+    // tests
     describe("Registrarse correcto", async function () {
 
         it("Todos los datos del registro son correctos y el usuario no está registrado aún", function (done) {
-            let request = {
-                body: {
-                    username: "paco",
-                    correo: "paco@gmail.com",
-                    password: "pacopass",
-                }
+            let nuevoUsuario = {
+                nombre: "paco",
+                correo: "paco@gmail.com",
+                pass: "pacopass",
             };
-            task.registroUsu(request, function (errors, result) {
+            task.registro(nuevoUsuario, function (errors, result) {
                 assert.equal(errors, null);
                 assert.notEqual(result, null);
                 done();
             });
-
-            done();
         });
 
     });
@@ -41,20 +35,16 @@ describe('hooks', function () {
     describe("Registrarse incorrecto", async function () {
 
         it("Ya hay un usuario registrado con el mismo nombre de usuario", function (done) {
-            let request = {
-                body: {
-                    username: "paco",
-                    correo: "lucia@gmail.com",
-                    password: "luciapass",
-                }
+            let nuevoUsuario = {
+                nombre: "paco",
+                correo: "lucia@gmail.com",
+                pass: "luciapass",
             };
-            task.registroUsu(request, function (errors, result) {
+            task.registro(nuevoUsuario, function (errors, result) {
                 assert.equal(errors, null);
                 assert.equal(result, false);
                 done();
             });
-
-            done();
         });
 
     });
@@ -62,20 +52,18 @@ describe('hooks', function () {
     describe("Registrarse incorrecto", async function () {
 
         it("Ya hay un usuario registrado con el mismo correo", function (done) {
-            let request = {
-                body: {
-                    username: "maria",
-                    correo: "paco@gmail.com",
-                    password: "mariapass",
-                }
+            let nuevoUsuario = {
+                nombre: "maria",
+                correo: "paco@gmail.com",
+                pass: "mariapass",
             };
-            task.registroUsu(request, function (errors, result) {
+            task.registro(nuevoUsuario, function (errors, result) {
                 assert.equal(errors, null);
                 assert.equal(result, false);
                 done();
             });
-            done();
         });
+
     });
 
 });
