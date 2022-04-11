8df8e9d63b0b62949367e97db18a750a852a9939
bea-espinar15 <beaesp01@ucm.es>
2022-04-11 10:49:36 +0200

    casos y delete_user añadidos

--- Codigo/Tests/Integracion/TINegocio-BD-RegistroUsuario.js
+++ Codigo/Tests/Integracion/TINegocio-BD-RegistroUsuario.js
@@ -12,21 +12,20 @@ const mysql = require('mysql');
 const config = require('../../js/config');
 const UserController = new controller();
 const pool = mysql.createPool(config.databaseConfig);
+const dao_aux = new dao(pool);
 
 
 
 describe('hooks', function () {
 
     after(function () {
-     
+        dao_aux.delete_user("paco@gmail.com", function (error, result) { });
     });
 
     // tests
     describe("Registrarse correcto", async function () {
 
         it("Todos los datos del registro son correctos y el usuario no está registrado aún", function (done) {
-
-            // mock para que no llame a render
             chai.request('http://localhost:3300')
             .post('/usuarios/registro_Usuario')
             .send({username:"paco", correo: "paco@gmail.com", password: "1234", password2: "1234"})
@@ -40,13 +39,11 @@ describe('hooks', function () {
 
     describe("Registrarse incorrecto", async function () {
 
-        it("Todos los datos del registro son correctos y el usuario no está registrado aún", function (done) {
-            // mock para que no llame a render
+        it("Ya hay un usuario registrado con el mismo nombre de usuario", function (done) {
             chai.request('http://localhost:3300')
             .post('/usuarios/registro_Usuario')
-            .send({username:"nene", correo: "nene@gmail.com", password: "1234", password2: "1234"})
+            .send({username:"paco", correo: "lucia@gmail.com", password: "1234", password2: "1234"})
             .end(function (err, response) {
-
                 expect(response).to.have.status(200);
                 done();
             })
@@ -54,18 +51,18 @@ describe('hooks', function () {
 
     });
 
-    // describe("Registrarse incorrecto", async function () {
+    describe("Registrarse incorrecto", async function () {
 
-    //     it("Ya hay un usuario registrado con el miismo correo", function (done) {
-    //         let request = {
-    //             body: {
-    //                 username: "maria",
-    //                 correo: "paco@gmail.com",
-    //                 password: "mariapass",
-    //             }
-    //         };
-    //         done();
-    //     });
-    // });
+        it("Ya hay un usuario registrado con el mismo correo", function (done) {
+            chai.request('http://localhost:3300')
+            .post('/usuarios/registro_Usuario')
+            .send({username:"maria", correo: "paco@gmail.com", password: "1234", password2: "1234"})
+            .end(function (err, response) {
+                expect(response).to.have.status(200);
+                done();
+            })
+        });
+
+    });
 
 });
