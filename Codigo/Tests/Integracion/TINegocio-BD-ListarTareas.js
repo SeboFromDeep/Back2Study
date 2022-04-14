3729c036de3ea00a242621a2974f7c5520e90d50
Noodlealvel <alvelasco2001@gmail.com>
2022-04-14 16:09:35 +0200

    TI negocio listar arreglados

--- Codigo/Tests/Integracion/TINegocio-BD-ListarTareas.js
+++ Codigo/Tests/Integracion/TINegocio-BD-ListarTareas.js
@@ -6,81 +6,79 @@ const expect = require('chai').expect;
 chai.use(chaiHttp);
 const app = require('../../app');
 
-//Controller Dependencies
-const controller = require("../../controller/userController");
-const mysql = require('mysql');
-const config = require('../../js/config');
-const UserController = new controller();
-const dao = require("../../js/userDAO");
-const pool = mysql.createPool(config.databaseConfig);
-const dao_aux = new dao(pool);
-
-
-
 describe('hooks', function () {
 
     // tests
     describe("Listar sin pruebas", async function () {
 
-        it("El usuario no tiene ninguna tarea", function (done) {
-            chai.request('http://localhost:3300')
-            .post('/tareas/taskList')
-            .send({userName:"userPrueba3", mail: "userPrueba3@email.com"})
-            .end(function (err, response) {
-                expect(response).to.have.status(200);
-                done();
-            })
+        it("El usuario no tiene ninguna tarea", async function () {
+
+            let agent = chai.request.agent('http://localhost:3300');
+            await agent.post('/usuarios/login_user')
+            .send({correo:'userPrueba3@email.com', password:'1234'})
+            .then((res) => {
+                    this.timeout(30000);
+                    agent.get('/tareas/taskList')
+                    .end((err, res) => {
+                        expect(res).to.have.status(200);
+                    });
+            });
         });
 
     });
 
     describe("Lista con solo tareas manuales ", async function () {
 
-        it("El usuario solo tiene tareas manuales", function (done) {
-            let request = {
-                session: {
-                    userName:"userPrueba6",
-                    mail: "userPrueba6@email.com",
-                }
+        it("El usuario solo tiene tareas manuales", async function () {
 
-            };
-            chai.request('http://localhost:3300')
-            .post('/tareas/taskList')
-            .send({request})
-            .end(function (err, response) {
-                expect(response).to.have.status(200);
-                done();
-            })
-        });
+        let agent = chai.request.agent('http://localhost:3300');
+         await agent.post('/usuarios/login_user')
+            .send({correo:'userPrueba6@email.com', password:'1234'})
+            .then((res) => {
+                    this.timeout(30000);
+                    agent.get('/tareas/taskList')
+                    .end((err, res) => {
+                        expect(res).to.have.status(200);
+                    });
+            });
+        })
 
     });
 
     describe("Lista con solo tareas programadas ", async function () {
 
-        it("El usuario solo tiene tareas programadas", function (done) {
-            chai.request('http://localhost:3300')
-            .post('/tareas/taskList')
-            .send({userName:"userPrueba2", mail: "userPrueba2@email.com"})
-            .end(function (err, response) {
-                expect(response).to.have.status(200);
-                done();
-            })
-        });
+        it("El usuario solo tiene tareas programadas", async function () {
+
+        let agent = chai.request.agent('http://localhost:3300');
+            await agent.post('/usuarios/login_user')
+            .send({correo:'userPrueba2@email.com', password:'1234'})
+            .then((res) => {
+                    this.timeout(30000);
+                    agent.get('/tareas/taskList')
+                    .end((err, res) => {
+                        expect(res).to.have.status(200);
+                    });
+            });
+        })
 
     });
 
 
     describe("Lista con ambos tipos de tareas", async function () {
 
-        it("El usuario tiene tareas de ambos tipos", function (done) {
-            chai.request('http://localhost:3300')
-            .post('/tareas/taskList')
-            .send({userName:"userPrueba1", mail: "userPrueba1@email.com"})
-            .end(function (err, response) {
-                expect(response).to.have.status(200);
-                done();
-            })
-        });
+        it("El usuario tiene tareas manuales y programadas", async function () {
+
+        let agent = chai.request.agent('http://localhost:3300');
+            await agent.post('/usuarios/login_user')
+            .send({correo:'userPrueba1@email.com', password:'1234'})
+            .then((res) => {
+                    this.timeout(30000);
+                    agent.get('/tareas/taskList')
+                    .end((err, res) => {
+                        expect(res).to.have.status(200);
+                    });
+            });
+        })
 
     });
 })
