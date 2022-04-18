4671e0cf805a6444c0236e349468d53a95131f47
Morwar22 <alejmo12@ucm.es>
2022-04-18 09:36:17 +0200

    cambio del hostname

--- Codigo/js/config.js
+++ Codigo/js/config.js
@@ -4,7 +4,7 @@
 
 module.exports ={
    databaseConfig:  {
-        host:"back2study-database.cp7iwtc61ifu.eu-west-3.rds.amazonaws.com",
+        host:"back2study-db.cp7iwtc61ifu.eu-west-3.rds.amazonaws.com",
         user:"admin",
         password: "Back2Studyadmin",
         database:"back2study"
