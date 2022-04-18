cf83b3ea6d0384df4dd28b247b69e3aa6d9c4810
bea-espinar15 <beaesp01@ucm.es>
2022-04-18 09:56:34 +0200

    Añadido insert_task para test

--- Codigo/Tests/Integracion/TINegocio-BD-BorrarTarea.js
+++ Codigo/Tests/Integracion/TINegocio-BD-BorrarTarea.js
@@ -17,21 +17,42 @@ const { getMaxListeners } = require("../../app");
 const pool = mysql.createPool(config.databaseConfig);
 const UserDao = new dao(pool);
 
+const t_dao = require("../../js/taskDAO");
+const dao_aux = new t_dao(pool);
+
 // tests
-describe("Borrar tarea", function () {
+describe('hooks', function () {
 
-    it("Borrar tarea", async function () {
+    after(function () {
+        // después de cada test insertamos la tarea que se ha borrado para poder ejecutarlos siempre
+        let tarea = {
+            id_tarea: 1,
+            nombre: 'Proyecto TP',
+            prioridad: 'ALTA',
+            categioria: '@TP@UNI',
+            id_usuario: 1,
+            fechafin: '2022-04-11',
+            fechaini: '2022-04-07',
+            tipo: 'p',
+        };
+        dao_aux.insert_task(tarea, function (error, result) { });
+    });
 
-        let agent = chai.request.agent(url);
-        await agent.post('/usuarios/login_user')
-            .send({ correo: 'userPrueba1@email.com', password: '1234' })
-            .then((res) => {
-                this.timeout(30000);
-                agent.get('/tareas/deleteTask/1/p')
-                    .end((err, res, body) => {
-                        expect(res).to.have.status(200);
-                    });
-            });
+    describe("Borrar tarea", function () {
+
+        it("Borrar tarea", async function () {
+            let agent = chai.request.agent(url);
+            await agent.post('/usuarios/login_user')
+                .send({ correo: 'userPrueba1@email.com', password: '1234' })
+                .then((res) => {
+                    this.timeout(30000);
+                    agent.get('/tareas/deleteTask/1/p')
+                        .end((err, res, body) => {
+                            expect(res).to.have.status(200);
+                        });
+                });
+        });
+
     });
 
 });
