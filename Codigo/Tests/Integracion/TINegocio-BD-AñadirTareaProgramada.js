ea60675380679a5c5160030010ce11dfcc87e7dc
Morwar22 <alejmo12@ucm.es>
2022-04-20 13:10:34 +0200

    arreglo en algunos test

--- Codigo/Tests/Integracion/TINegocio-BD-AñadirTareaProgramada.js
+++ Codigo/Tests/Integracion/TINegocio-BD-AñadirTareaProgramada.js
@@ -19,6 +19,17 @@ const UserDao = new dao(pool);
 
 // tests
 describe("Añadir Tarea Programada", function () {
+    before(function (){
+        it("Todos los datos del registro son correctos y el usuario no está registrado aún", function (done) {
+            chai.request('http://localhost:3300')
+            .post('/usuarios/registro_Usuario')
+            .send({username:"prueba", correo: "prueba@gmail.com", password: "1234", password2: "1234"})
+            .end(function (err, response) {
+                expect(response).to.have.status(200);
+                done();
+            })
+        });
+    });
 
     it("Tarea Programada correcta", async function () {
 
@@ -26,9 +37,8 @@ describe("Añadir Tarea Programada", function () {
         await agent.post('/usuarios/login_user')
             .send({ correo: 'prueba@gmail.com', password: '1234' })
             .then((res) => {
-                this.timeout(30000);
-                console.log(res.body);
-                agent.post('/tareas/add_scheduled_task')
+                    expect(res).to.have.status(200);
+                    agent.post('/tareas/add_scheduled_task')
                     .send({ nombre: "prueba", prioridad: "Media", tipo: "Diaria", categoria: "@prueba", horas: "1", fechaIni: "12/04/2022", fechaFin: "13/04/2022" })
                     .redirects(0)
                     .end((err, res, body) => {
@@ -43,12 +53,12 @@ describe("Añadir Tarea Programada", function () {
             await agent.post('/usuarios/login_user')
             .send({correo:'prueba@gmail.com', password:'1234'})
             .then((res) => {
-                    this.timeout(30000);
-                    console.log(res.boody);
+                    expect(res).to.have.status(200);
                     agent.post('/tareas/add_scheduled_task')
                     .send({nombre:"prueba", prioridad: "", tipo:"Semanal", categoria: "@prueba", horas: "1", fechaIni:"", fechaFin:""})
                     .redirects(0)
                     .end((err, res, body) => {
+
                         expect(res).to.have.status(302);
                     });
             });
@@ -60,16 +70,14 @@ describe("Añadir Tarea Programada", function () {
             await agent.post('/usuarios/login_user')
             .send({correo:'prueba@gmail.com', password:'1234'})
             .then((res) => {
-                    this.timeout(30000);
-                    console.log(res.boody);
+                    expect(res).to.have.status(200);
                     agent.post('/tareas/add_scheduled_task')
-                    .send({nombre:"prueba", prioridad: "Media", tipo:"Diaria", categoria: "@prueba", horas: "0", fechaIni:"12/04/2022" , fechaFin:"13/04/2022"})
+                    .send({nombre:"prueba", prioridad: "Media", tipo:"Diaria", categoria: "@prueba", horas: "0", fechaIni:"11/04/2022" , fechaFin:"10/04/2022"})
                     .redirects(0)
                     .end((err, res, body) => {
+
                         expect(res).to.have.status(302);
                 });
         });
-
     });
-
 });
